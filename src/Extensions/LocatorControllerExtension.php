43df6137f8d2763ef3ef0fd7abf54c21f8aae58a
mak001 <mak001001@gmail.com>
2017-11-14 09:36:25 -0600

    Started adding better settings loading

--- src/Extensions/LocatorControllerExtension.php
+++ src/Extensions/LocatorControllerExtension.php
@@ -17,7 +17,30 @@ class LocatorControllerExtension extends Extension
      */
     public function onBeforeInit()
     {
+        // stops script from loading
         Requirements::block('jquery-locator');
+
+        $radii = $this->owner->getShowRadius() ? $this->owner->getRadii() : [];
+        $categories = $this->owner->getCategories() ?
+            $this->owner->getCategories()->map('ID','Name') : [];
+
+        $settings = array(
+            'radii' => $radii,
+            'categories' => $categories,
+            'unit' => $this->owner->Unit,
+            'limit' => $this->owner->getLimit(),
+            // 'clusters' => $this->owner->getClusters(),
+            'clusters' => false,
+            'infoWindowTemplate' => $this->owner->getInfoWindowTemplateContent(),
+            'listTemplate' => $this->owner->getListTemplateContent(),
+        );
+
+        $this->owner->extend('updateSettings', $settings);
+
+        Requirements::javascriptTemplate(
+            "dynamic/silverstrpe-locator-react: templates/settings.template.js",
+            $settings,
+            'react-locator');
     }
 
 }
---
+++ templates/settings.template.js
@@ -0,0 +1,11 @@
+window.dynamic_locator = {
+  'radii': $radii,
+  'categories': $categories,
+  'unit': $unit,
+  'limit': $limit,
+  'clusters': $clusters,
+  'infoWindowTemplate': $infoWindowTemplate,
+  'listTemplate': $listTemplate
+};
+
+console.log(window.dynamic_locator);
