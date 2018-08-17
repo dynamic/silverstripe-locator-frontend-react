6f80fdb707bcc13c09c1114e30580cfe3a2012bd
mak001 <mak001001@gmail.com>
2018-08-17 11:05:53 -0500

    Hopefully fixed javascript tests

--- client/src/tests/setup.js
+++ client/src/tests/setup.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
 
