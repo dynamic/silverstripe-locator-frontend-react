96263095cd88805056391b57d31c5dcf5cf5ae34
mak001 <mak001001@gmail.com>
2020-01-30 10:04:18 -0600

    Fixed code climate eslint issues

--- client/build/js/resolve.js
+++ client/build/js/resolve.js
@@ -1,7 +1,12 @@
 /**
  * @param {stirng} SRC_JS Path to javascript source
  */
-module.exports = (ENV, { ROOT, MODULES, SRC, SRC_JS }) => (
+module.exports = (ENV, {
+  ROOT,
+  MODULES,
+  SRC,
+  SRC_JS,
+}) => (
   {
     modules: [ROOT, SRC, SRC_JS, MODULES],
     extensions: ['.js', '.jsx', '.json'],
